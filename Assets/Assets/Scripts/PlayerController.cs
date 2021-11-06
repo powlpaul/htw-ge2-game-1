54033d62960c8234ee6c52000a77fc644db7b89d
LennoxCode <leonard.valentin1999@gmail.com>
2021-11-06 14:11:11 +0100

    Added primitve level

---
+++ Assets/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed = 5;
+    [SerializeField] private float jumpSpeed = 5;
+    [SerializeField] private float jumpHeight = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+        }
+    }
+}
