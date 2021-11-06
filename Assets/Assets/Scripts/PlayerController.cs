3025681ba2b33fba5a1229f2400d37da26375a7f
LennoxCode <leonard.valentin1999@gmail.com>
2021-11-06 14:29:33 +0100

    added primitve Player Controller

--- Assets/Assets/Scripts/PlayerController.cs
+++ Assets/Assets/Scripts/PlayerController.cs
@@ -7,18 +7,29 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float speed = 5;
     [SerializeField] private float jumpSpeed = 5;
     [SerializeField] private float jumpHeight = 5;
+
+    private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+      
         if (Input.GetKeyDown(KeyCode.Space))
         {
-
+            rb.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            rb.AddForce(Vector2.right * speed * Time.deltaTime);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            rb.AddForce(Vector2.left * speed * Time.deltaTime);
         }
     }
 }
