ccad66ab1fb6ad9c2655fe30592a6cb2ecd4c8d2
LennoxCode <leonard.valentin1999@gmail.com>
2021-11-22 21:33:13 +0100

    added primitve bounce

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    [SerializeField] private double bouncePower = 0;
+    private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("collision");
+        Vector2 newForce = rb.velocity.normalized * -1;
+        rb.AddForce(newForce * 1000);
     }
 }
