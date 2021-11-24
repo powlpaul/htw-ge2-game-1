57b60cb5f11df1ea6e136a0a2191dd43f16b088c
LennoxCode <leonard.valentin1999@gmail.com>
2021-11-24 20:23:14 +0100

    added primitive scoring

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,13 +6,13 @@ public class GameController : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] private GameObject ball;
-    [SerializeField] private GameObject ballSpawnPlayer1;
-    [SerializeField] private GameObject ballSpawnPlayer2;
+    [SerializeField] private GameObject ballSpawnPlayer1; [SerializeField] private GameObject ballSpawnPlayer2;
+    private int player1Score = 0; private int player2Score = 0;
     private Vector2 center = new Vector2(0, 0);
    
     void Start()
     {
-        
+        ball.transform.position = ballSpawnPlayer1.transform.position;
     }
 
     // Update is called once per frame
@@ -22,6 +22,22 @@ public class GameController : MonoBehaviour
     }
     public void BallHitGround()
     {
-        ball.transform.position = ballSpawnPlayer1.transform.position;
+        if(ball.transform.position.x < center.x)
+        {
+            player2Score++;
+            ball.transform.position = ballSpawnPlayer1.transform.position;
+        }
+        else
+        {
+            player1Score++;
+            ball.transform.position = ballSpawnPlayer2.transform.position;
+        }
+        Debug.Log("Player 1 Score: " + player1Score + " \n player 2 Score: " + player2Score);
+
+    }
+    public void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(ballSpawnPlayer1.transform.position, 0.5f);
+        Gizmos.DrawSphere(ballSpawnPlayer2.transform.position, 0.5f);
     }
 }
