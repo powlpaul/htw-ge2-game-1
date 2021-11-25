d050ed56edefd743d9eea1cfe02ca72eb903de55
LennoxCode <leonard.valentin1999@gmail.com>
2021-11-25 22:14:51 +0100

    added lose condition

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,19 +6,38 @@ public class GameController : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] private GameObject ball;
+    [SerializeField] private GameObject Player1;[SerializeField] private GameObject Player2;
     [SerializeField] private GameObject ballSpawnPlayer1; [SerializeField] private GameObject ballSpawnPlayer2;
     private int player1Score = 0; private int player2Score = 0;
     private Vector2 center = new Vector2(0, 0);
-   
+    private float gameCountDown;
+    
     void Start()
     {
-        ball.transform.position = ballSpawnPlayer1.transform.position;
+        Initialize(60f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        gameCountDown -= 1 * Time.deltaTime;
+        if (gameCountDown <= 0) finalize();
+    }
+    private void Initialize(float gameDuration)
+    {
+        Time.timeScale = 1;
+        gameCountDown = gameDuration;
+        ball.transform.position = ballSpawnPlayer1.transform.position;
+        Player1.transform.position = new Vector2(ballSpawnPlayer1.transform.position.x, -2.8f);
+        Player2.transform.position = new Vector2(ballSpawnPlayer2.transform.position.x, -2.8f);
+    }
+
+    private void finalize()
+    {
+        string message;
+        message = player1Score > player2Score ? "Player 1 won" : "Player 2 won";
+        Debug.Log(message);
+        Time.timeScale = 0;
     }
     public void BallHitGround()
     {
@@ -33,7 +52,7 @@ public class GameController : MonoBehaviour
             ball.transform.position = ballSpawnPlayer2.transform.position;
         }
         ball.SetActive(false);
-        Waiter.Wait(3, () => {
+        Waiter.Wait(2, () => {
             // Just to make sure by the time we're back to activate it, it still exists and wasn't destroyed.
             if (ball != null)
                 ball.SetActive(true);
