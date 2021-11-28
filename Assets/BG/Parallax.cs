280e93fd5689acd9ab02273a6823f25c781e2a9f
Paul Klingberg <pklingberg@gmail.com>
2021-11-28 18:20:56 +0100

    First Level
    Leven with two platforms and parallaxing background

---
+++ Assets/BG/Parallax.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    private float length, startpos;
+    public GameObject cam;
+    public float parallaxEffect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startpos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float distance = (cam.transform.position.x * parallaxEffect);
+        transform.position = new Vector3(startpos + distance, transform.position.y, transform.position.z);
+
+    }
+}
