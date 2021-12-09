1c29e3268b4a240d1625e16faea2d63d2c8795c8
FlyingBaum <kristgsus@gmail.com>
2021-12-09 01:53:01 +0100

    Birb animations complete, jump / movement complete

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,10 +6,11 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed = 100;
-    [SerializeField] private float jumpSpeed = 1.5f;
+    [SerializeField] private float jumpSpeed = 5;
     [SerializeField] private float fallSpeed = 20;
     [SerializeField] private float firstJumpHeight = 8;
     [SerializeField] private float jumpMaxFloorDistance = 4;
+    [SerializeField] private Animator animator;
 
     private AudioController audioController;
     private Rigidbody2D rb;
@@ -38,6 +39,7 @@ public class PlayerController : MonoBehaviour
             rb.gravityScale = fallSpeed;
         }
 
+        //velocity set to zero if no movement to the right / left
         if(inputValue != 0)
         {
             rb.AddForce(Vector2.right * inputValue * speed * Time.deltaTime);
@@ -48,14 +50,34 @@ public class PlayerController : MonoBehaviour
         }
 
 
-        //Add force if jump is held for less than 30 frames
+        //Add force if jump is held for less than 15 frames
         if (keyHeld && jumpFrameCount < 15)
         {
-            rb.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
+            rb.AddForce(Vector2.up * 1.2f, ForceMode2D.Impulse);
             jumpFrameCount++;
         }
 
         transform.rotation = Quaternion.Euler(Vector3.zero);
+
+        //determines whether player should look left or right
+        if (inputValue < 0)
+        {
+            transform.localScale = new Vector3(0.1f, transform.localScale.y, transform.localScale.z);
+        }
+        else if (inputValue > 0)
+        {
+            transform.localScale = new Vector3(-0.1f, transform.localScale.y, transform.localScale.z);
+        }
+
+        animator.SetFloat("Speed", Mathf.Abs(inputValue));
+        if(!JumpingAllowed())
+        {
+            animator.SetBool("isJumping", true);
+        }
+        else
+        {
+            animator.SetBool("isJumping", false);
+        }
     }
 
     //Eventhandler for jump input. Is called when jump is initially pressed and released.
@@ -80,6 +102,7 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    //get horizontal direction for walking
     public void Walk(InputAction.CallbackContext context)
     {
         audioController.PlayWalkingSound();
