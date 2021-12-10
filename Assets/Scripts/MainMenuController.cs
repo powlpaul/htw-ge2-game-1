e2420e4b59df2d975629dcd961a6bf573786d83f
LennoxCode <leonard.valentin1999@gmail.com>
2021-12-10 18:53:23 +0100

    added functionality for volume buttons

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour
 {
+    
     [SerializeField] private GameObject mainMenu;
     [SerializeField] GameObject mainMenuItems;
     [SerializeField] GameObject optionMenuItems;
+    [SerializeField] private Text MusicVolumeDisplay;
+    [SerializeField] private Text EffectsVolumeDisplay;
+    private float MusicVolume = 10;
+    private float EffectsVolume = 10;
     // Start is called before the first frame update
     void Start()
     {
@@ -40,5 +46,41 @@ public class MainMenuController : MonoBehaviour
     {
         mainMenuItems.SetActive(false);
         optionMenuItems.SetActive(true);
+    }
+
+    public void BackOnClick()
+    {
+        mainMenuItems.SetActive(true);
+        optionMenuItems.SetActive(false);
+    }
+
+    public void MusicButtonClick(bool right)
+    {
+        if(right && MusicVolume < 10)
+        {
+            MusicVolume++;
+           
+        }else if(!right && MusicVolume > 0)
+        {
+            MusicVolume--;
+        }
+
+        MusicVolumeDisplay.text = "" +  MusicVolume;
+    }
+
+    public void EffectsButtonClick(bool right)
+    {
+        Debug.Log("effect button was clicked: " + right);
+        if (right && EffectsVolume < 10)
+        {
+            EffectsVolume++;
+
+        }
+        else if (!right && EffectsVolume > 0)
+        {
+            EffectsVolume--;
+        }
+
+        EffectsVolumeDisplay.text = "" + EffectsVolume;
     }
 }
